87eed7061321c6bdd70ef593deaa3adc78128c2c
Jake Hamilton <Hamilton456b@gmail.com>
2023-03-20 12:46:05 -0500

    Mess with cameras

--- Assets/Scripts/HeadMovement.cs
+++ Assets/Scripts/HeadMovement.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class HeadMovement : MonoBehaviour
 {
-    public GameObject player;
+    enum Position { Paper, Professor, Phone, Classmate}
+
+    public GameObject player, phone, mainCamera;
     private float smooth = 30f;
 
     void Start()
     {
         player = this.gameObject;
     }
-
     public void lookDown()
     {
         StartCoroutine(lookDownCorotine());
     }
-
+    public void lookLeft()
+    {
+        StartCoroutine(lookLeftCorotine());
+    }
+    public void lookRight()
+    {
+        StartCoroutine(lookDownCorotine());
+    }
     public void lookUp()
     {
         StartCoroutine(lookUpCorotine());
@@ -35,6 +43,8 @@ public class HeadMovement : MonoBehaviour
 
             player.transform.rotation = Quaternion.Slerp(player.transform.rotation, target, Time.deltaTime * smooth);
 
+            //phone.transform.rotation = Quaternion.Slerp(phone.transform.rotation, player.transform.rotation, Time.deltaTime * smooth);
+
             yield return new WaitForSeconds(.01f);
         }
     }
@@ -46,6 +56,22 @@ public class HeadMovement : MonoBehaviour
             //Finds transform from axis
             float tiltAroundX = 0f;
             float tileAroundY = 0f;
+
+            //Rotate to target position
+            Quaternion target = Quaternion.Euler(tiltAroundX, tileAroundY, 0);
+
+            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, target, Time.deltaTime * smooth);
+
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+    IEnumerator lookLeftCorotine()
+    {
+        for (int i = 0; i < 70; i++)
+        {
+            //Finds transform from axis
+            float tiltAroundX = 27.32f;
+            float tileAroundY = 43.63f;
 
             //Rotate to target position
             Quaternion target = Quaternion.Euler(tiltAroundX, tileAroundY, 0);
